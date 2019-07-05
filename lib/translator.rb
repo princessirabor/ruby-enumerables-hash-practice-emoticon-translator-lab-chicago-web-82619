a5792a91e06c97ae77dad22690bf9b65d2e1c27f
Princess Irabor <irabor.princess1@gmail.com>
2019-07-05 21:27:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,12 @@ def load_library(dir)
   # code goes here
   result ={}
   cnf = YAML::load_file(File.join(__dir__, 'emoticons.yml'))
-  p cnf
+  
+  cnf.each do |(key,value)|
+    (value.length-1).times do |index|
+      result[value[index]] =
+    end
+  end
   
   #cnf["get_meaning"] = {}
   #cnf["get_emoticon"] = {}
